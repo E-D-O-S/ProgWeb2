9e6fd5ac87571855cd9ac8495d9c527820d32632
eduolmo <91472785+eduolmo@users.noreply.github.com>
2022-01-28 18:03:38 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -5,11 +5,13 @@ document.body.background = 'img/aquarela.jpg';
 function criaTabuleiro() {
     const tamanho = 8;
     let tabela = document.createElement('table');
-
     tabela.style.borderStyle = 'solid';
     tabela.style.borderSpacing = 0;
     tabela.style.margin = 'auto';
     tabela.style.borderColor = 'orange';
+    tabela.style.position = 'absolute';
+    tabela.style.top = '25%';
+    tabela.style.left = '35%';
 
     for (let i = 0; i < tamanho; i++) {
         let linha = document.createElement('tr');
@@ -22,6 +24,7 @@ function criaTabuleiro() {
             if (i % 2 == j % 2) {
                 celula.style.backgroundColor = '#00BECC';
                 celula.classList.add("container");
+                celula.id = `${i}${j}`
                 if (i * 8 + j <= 24) {
                     celula.append(criaPeca('black'));
                 } else if (i * 8 + j >= 40) {
@@ -49,9 +52,8 @@ const draggables = document.querySelectorAll(".draggable");
 const containers = document.querySelectorAll(".container");
 
 draggables.forEach(draggable => {
-    draggable.addEventListener("dragstart", () => {
+    draggable.addEventListener("dragstart", e => {
         draggable.classList.add("dragging");
-        
     });
     draggable.addEventListener("dragend", () => {
         draggable.classList.remove("dragging");
@@ -62,8 +64,13 @@ containers.forEach(container => {
     container.addEventListener("dragover", e => {
         e.preventDefault();
     });
-    container.addEventListener("drop", () => {
+    container.addEventListener("drop", e => {
         const draggable = document.querySelector(".dragging");
-        container.appendChild(draggable);
+        let bloco = e.target.id
+        let casa = document.querySelector('.dragging').parentElement.id
+        //limitando movimento das pecas
+        if(bloco[0] != casa[0] && 2 > bloco[0]-casa[0] && bloco[0]-casa[0] > -2 && bloco[1]-casa[1] < 2 && bloco[1]-casa[1] > -2){
+            container.appendChild(draggable);
+        }
     });
 });
